e856a16d24ebbe77d173b1aaee65c2c4087b2bbf
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-02-23 22:41:56 -0800

    fixed command line parsing issue, and type issue

--- sqrt.py
+++ sqrt.py
@@ -3,15 +3,19 @@
 
 """Calculates and prints the square root of a given number.
 Usage: sqrt.py <number>
+
 Options:
 <number>    Number for which the square root should be calculated
 """
-  
+
 from docopt import docopt
 import math
 
+opt = docopt(__doc__)
+
 def main(number):
-  print(square_root(number))
+  number = int(number)
+  print(math.sqrt(number))
 
 if __name__ == "__main__":
   main(opt["<number>"])
